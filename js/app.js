e9bc5a1bb15337865a697d6ca936aba3a948f5c4
baconman465 <ps24acolburn@efcts.us>
2023-04-18 10:18:07 -0400

    green lol

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let el_nav = document.getElementById("nav");
+
+el_nav.innerHTML = "<div class='header_container'><div class='header'><div class='nav_left'><a href='index.html'>Home</a><a href='#'></a></div><div class='header_img'><img src='images/really cool dinosaur.jpg' height='100rem' class='header_img'></div><div class='nav_right'><a href='#'></a><a href='#'></a></div></div></div>"
+
+/*
+<div class='header_container'>
+    <div class='header'>
+        <div class='nav_left'>
+            <a href='index.html'>Home</a>
+            <a href='#'></a>
+        </div>
+    
+        <div class='header_img'>
+            <img src='images/really cool dinosaur.jpg' height='100rem' class='header_img'>
+        </div>
+    
+        <div class='nav_right'>
+            <a href='#'></a>
+            <a href='#'></a>
+        </div>
+    </div>
+</div>
+*/
