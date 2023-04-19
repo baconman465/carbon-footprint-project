8fcdfbb87a736a14e6e06f463732e686de2ae299
baconman465 <ps24acolburn@efcts.us>
2023-04-19 10:28:44 -0400

    spin nav

--- js/app.js
+++ js/app.js
@@ -1,23 +1,14 @@
 let el_nav = document.getElementById("nav");
 
-el_nav.innerHTML = "<div class='header_container'><div class='header'><div class='nav_left'><a href='index.html'>Home</a><a href='#'></a></div><div class='header_img'><img src='images/really cool dinosaur.jpg' height='100rem' class='header_img'></div><div class='nav_right'><a href='#'></a><a href='#'></a></div></div></div>"
+el_nav.innerHTML = "<div class='header_container'><div class='header'><div class='nav_left'><a href='index.html'>Home</a><a href='#'>placeholder</a><a href='#'>placeholder</a></div><div class='header_img'><img src='images/really cool dinosaur.jpg' height='80rem' class='header_img rotate'></div><div class='nav_right'><a href='#'>placeholder</a><a href='#'>placeholder</a><a href='#'>placeholder</a></div><button class='closeButton' onclick='removeNav()'>Close</button></div></div>";
 
-/*
-<div class='header_container'>
-    <div class='header'>
-        <div class='nav_left'>
-            <a href='index.html'>Home</a>
-            <a href='#'></a>
-        </div>
-    
-        <div class='header_img'>
-            <img src='images/really cool dinosaur.jpg' height='100rem' class='header_img'>
-        </div>
-    
-        <div class='nav_right'>
-            <a href='#'></a>
-            <a href='#'></a>
-        </div>
-    </div>
-</div>
-*/
+
+let navCode = el_nav.innerHTML;
+
+function removeNav(){
+    el_nav.innerHTML = "<button class = 'openButton' onclick='addNav()'>Open</button>";
+}
+
+function addNav(){
+    el_nav.innerHTML = navCode;
+}
