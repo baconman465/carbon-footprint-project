b5275548c4f8966f31a34aacb4b5dc99cb042d54
baconman465 <ps24acolburn@efcts.us>
2023-04-20 10:11:08 -0400

    a

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let el_nav = document.getElementById("nav");
 
-el_nav.innerHTML = "<div class='header_container'><div class='header'><div class='nav_left'><a href='index.html'>Home</a><a href='#'>placeholder</a><a href='#'>placeholder</a></div><div class='header_img'><img src='images/really cool dinosaur.jpg' height='80rem' class='header_img rotate'></div><div class='nav_right'><a href='#'>placeholder</a><a href='#'>placeholder</a><a href='#'>placeholder</a></div><button class='closeButton' onclick='removeNav()'>Close</button></div></div>";
+el_nav.innerHTML = "<div class='header_container'><div class='header'><div class='nav_left'><a href='index.html'>Home</a><a href='#'>placeholder</a><a href='#'>placeholder</a></div><div class='header_img'><img src='images/really cool dinosaur.jpg' height='80rem' class='header_img rotate'></div><div class='nav_right'><a href='#'>placeholder</a><a href='#'>placeholder</a><a href='work cited.html'>Work Cited</a></div><button class='closeButton' onclick='removeNav()'>Close</button></div></div>";
 
 
 let navCode = el_nav.innerHTML;
